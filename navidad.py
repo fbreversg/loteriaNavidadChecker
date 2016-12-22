a86f20d73ca9a6503aca67a82dc37f3d48ef9be4
Paco Brevers <francisco.brevers@tecnilogica.com>
2016-12-22 10:15:57 +0100

    Perreo maximo con un sleep y bucle infinito.

--- navidad.py
+++ navidad.py
@@ -5,21 +5,26 @@ import time
 numeros = (33131,66859,67713,80472,46982,53243,11618)
 resultados = []
 
-for numero in numeros:
-    response = urllib.request.urlopen('http://api.elpais.com/ws/LoteriaNavidadPremiados?n='+str(numero))
-    datos = json.loads(response.read().decode('utf8').replace('busqueda=', ''))
-    if datos['error'] == 0:
-        resultados.append(datos)
-    else:
-        while datos['error'] != 0:
-            response = urllib.request.urlopen('http://api.elpais.com/ws/LoteriaNavidadPremiados?n='+str(numero))
-            datos = json.loads(response.read().decode('utf8').replace('busqueda=', ''))
-            if datos['error'] == 0:
-                resultados.append(datos)
-            time.sleep(0.5)
+while True:
 
-for resultado in resultados:
-    if resultado['premio'] > 0:
-        print('PREMIO en %s con %s' % (resultado['numero'], resultado['premio']))
-    else:
-        print('MOJON en %s con %s' % (resultado['numero'], resultado['premio']))
+    for numero in numeros:
+        response = urllib.request.urlopen('http://api.elpais.com/ws/LoteriaNavidadPremiados?n='+str(numero))
+        datos = json.loads(response.read().decode('utf8').replace('busqueda=', ''))
+        if datos['error'] == 0:
+            resultados.append(datos)
+        else:
+            while datos['error'] != 0:
+                response = urllib.request.urlopen('http://api.elpais.com/ws/LoteriaNavidadPremiados?n='+str(numero))
+                datos = json.loads(response.read().decode('utf8').replace('busqueda=', ''))
+                if datos['error'] == 0:
+                    resultados.append(datos)
+                time.sleep(0.5)
+
+    for resultado in resultados:
+        if resultado['premio'] > 0:
+            print('PREMIO en %s con %s' % (resultado['numero'], resultado['premio']))
+        else:
+            print('MOJON en %s con %s' % (resultado['numero'], resultado['premio']))
+
+    print("A ver si a la siguiente")
+    time.sleep(60)
