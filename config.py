919699796ac1bb584f827fc003d254541298b6bd
Paco Brevers <francisco.brevers@tecnilogica.com>
2018-12-20 18:30:10 +0100

    Eliminado antiguo script y config.

--- config.py
+++
@@ -1,8 +0,0 @@
-""" Configuracion para el script de comprobacion de loteria de navidad """
-
-# Numeros a comprobar <numero:importe>
-numeros = {1: 20, 2: 10, 3: 30, 4: 50, 5: 1}
-
-# Configuracion de slack (OPCIONAL)
-slack_token = 'TOKEN_DE_SLACK'     # https://api.slack.com/custom-integrations/legacy-tokens
-slack_user = '@usuario_slack'   # tu nombre usuario de slack con @ (Trick, si pones #canal puedes mandarlo a un canal que quieras BAJO TU RESPONSABILIDAD!!!!)
